c7c6d3efd1da793014e43fe09ea4758e992b9ec9
Ronsen <ronsen@startrek.or.id>
2025-04-12 15:31:52 +0700

    fix: encoder

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 use Intervention\Image\Drivers\Imagick\Driver;
+use Intervention\Image\Drivers\Imagick\Encoders\JpegEncoder;
+use Intervention\Image\Drivers\Imagick\Encoders\PngEncoder;
+use Intervention\Image\Drivers\Imagick\Encoders\WebpEncoder;
 use Intervention\Image\ImageManager;
 
 class FileController extends Controller
@@ -18,15 +22,28 @@ class FileController extends Controller
 			'file' => ['image', 'max:20480'],
 		]);
 
+		$quality = (int) $request->input('quality');
+
 		$file = Storage::put(date('Ymd'), $request->file);
 		$path = Storage::path($file);
 		$url = Storage::url($file);
+		$extension = File::extension($file);
 
 		$manager = new ImageManager(new Driver());
 		$image = $manager->read($path);
-		$image->scaleDown($image->width() * (int) $request->input('scale') / 100);
-		$image->toJpeg((int) $request->input('quality'));
-		$image->save($path);
+		$image->scaleDown(
+			width: $image->width() * (int) $request->input('scale') / 100,
+			height: $image->height() * (int) $request->input('scale') / 100,
+		);
+		
+		$encoder = match ($extension) {
+			'png' => new PngEncoder(),
+			'webp' => new WebpEncoder(quality: $quality),
+			default => new JpegEncoder(quality: $quality),
+		};
+
+		$image->encode($encoder)->save($path);
+
 		$image = $manager->read($path); // reread
 
 		return Inertia::render('Upload', [
