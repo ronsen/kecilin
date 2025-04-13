26388752d7ac24d455b547e62448b8920da5fb3b
Ronsen <ronsen@startrek.or.id>
2025-04-13 10:21:00 +0700

    add: api

---
+++ app/Http/Controllers/Api/FileController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Utils\FileUtil;
+
+class FileController extends Controller
+{
+	public function store(Request $request): JsonResponse
+	{
+		$request->validate([
+			'file' => ['required', 'image', 'max:20480'],
+		]);
+
+		$scale = $request->input('scale') ? (int) $request->input('scale') / 100 : 1;
+		$quality = $request->input('quality') ? (int) $request->input('quality') : 0.8;
+
+		$result = FileUtil::upload($request->file, $scale, $quality);
+
+		return response()->json($result);
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/upload', [\App\Http\Controllers\Api\FileController::class, 'store']);
