2c0afa14943bdc6b52e452093150a3f59befc7dc
Ronsen <ronsen@startrek.or.id>
2025-04-13 10:11:58 +0700

    refactor

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,63 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
-use Intervention\Image\Drivers\Imagick\Driver;
-use Intervention\Image\Drivers\Imagick\Encoders\JpegEncoder;
-use Intervention\Image\Drivers\Imagick\Encoders\PngEncoder;
-use Intervention\Image\Drivers\Imagick\Encoders\WebpEncoder;
-use Intervention\Image\ImageManager;
+use App\Utils\FileUtil;
 
 class FileController extends Controller
 {
 	public function store(Request $request): Response
 	{
 		$request->validate([
-			'file' => ['image', 'max:20480'],
+			'file' => ['required', 'image', 'max:20480'],
 		]);
 
-		$quality = (int) $request->input('quality');
 		$scale = (int) $request->input('scale') / 100;
+		$quality = (int) $request->input('quality');
 
-		$file = Storage::put(date('Ymd'), $request->file);
-		$path = Storage::path($file);
-		$url = Storage::url($file);
-		$extension = File::extension($file);
-
-		$manager = new ImageManager(new Driver());
-		$image = $manager->read($path);
-		$image->scaleDown(
-			width: $image->width() * $scale,
-			height: $image->height() * $scale,
-		);
-		
-		$encoder = match ($extension) {
-			'png' => new PngEncoder(),
-			'webp' => new WebpEncoder(quality: $quality),
-			default => new JpegEncoder(quality: $quality),
-		};
-
-		$image->encode($encoder)->save($path);
-
-		$image = $manager->read($path); // reread
+		$result = FileUtil::upload($request->file, $scale, $quality);
 
 		return Inertia::render('Upload', [
-			'file' => $file,
-			'url' => $url,
-			'fileName' => $image->exif('FILE.FileName'),
-			'fileSize' => $image->exif('FILE.FileSize'),
-			'mimeType' => $image->exif('FILE.MimeType'),
-			'height' => $image->exif('COMPUTED.Height'),
-			'width' => $image->exif('COMPUTED.Width'),
+			'file' => $result['file'],
+			'url' => $result['url'],
+			'fileName' => $result['fileName'],
+			'fileSize' => $result['fileSize'],
+			'mimeType' => $result['mimeType'],
+			'height' => $result['height'],
+			'width' => $result['width'],
 		]);
 	}
 
 	public function delete(Request $request): RedirectResponse
 	{
-		Storage::delete($request->input('file'));
+		FileUtil::delete($request->input('file'));
 
 		return redirect('/')->with('message', 'File was successfully deleted from server.');
 	}
---
+++ app/Utils/FileUtil.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Utils;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Drivers\Imagick\Driver;
+use Intervention\Image\Drivers\Imagick\Encoders\JpegEncoder;
+use Intervention\Image\Drivers\Imagick\Encoders\PngEncoder;
+use Intervention\Image\Drivers\Imagick\Encoders\WebpEncoder;
+use Intervention\Image\ImageManager;
+
+class FileUtil
+{
+	public static function upload(UploadedFile $uploadedFile, int $scale, int $quality): array
+	{
+		$file = Storage::put(date('Ymd'), $uploadedFile);
+		$path = Storage::path($file);
+		$url = Storage::url($file);
+		$extension = File::extension($file);
+
+		$manager = new ImageManager(new Driver());
+		$image = $manager->read($path);
+		$image->scaleDown(
+			width: $image->width() * $scale,
+			height: $image->height() * $scale,
+		);
+		
+		$encoder = match ($extension) {
+			'png' => new PngEncoder(),
+			'webp' => new WebpEncoder(quality: $quality),
+			default => new JpegEncoder(quality: $quality),
+		};
+
+		$image->encode($encoder)->save($path);
+
+		$image = $manager->read($path); // reread
+
+		return [
+			'file' => $file,
+			'url' => $url,
+			'fileName' => $image->exif('FILE.FileName'),
+			'fileSize' => $image->exif('FILE.FileSize'),
+			'mimeType' => $image->exif('FILE.MimeType'),
+			'height' => $image->exif('COMPUTED.Height'),
+			'width' => $image->exif('COMPUTED.Width'),
+		];
+	}
+
+	public static function delete(string $file): void
+	{
+		Storage::delete($file);
+	}
+}
